16d073bc0978fb26ea32b60012d1c66c95ada018
Muhamad Panji Wiramanik <panjiwiramanik70@gmail.com>
2022-08-16 14:58:35 +0700

    Update SitemapService.php

--- src/SitemapService.php
+++ src/SitemapService.php
@@ -35,11 +35,21 @@ class SitemapService
      *
      * @param $links
      */
-    private function bulkAdd($links)
+    private function bulkAdd($links, $urlCrawlSource = null)
     {
-        foreach($links as $link){
-            $this->collection->add($link);
+        foreach ($links as $link) {
+            $urlCrawlTarget = parse_url($link, PHP_URL_HOST);
+            if ($urlCrawlSource == $urlCrawlTarget) {
+                $this->collection->add($link);
+            }
         }
+
+        uasort($this->collection->links, 'sortByLength');
+        $this->collection->links = array_values(array_unique($this->collection->links));
+    }
+
+    private function sortByLength($a, $b) {
+        return strlen($a) - strlen($b);
     }
 
     /**
@@ -51,15 +61,19 @@ class SitemapService
      */
     public function crawl($url)
     {
+        $urlCrawlSource = parse_url($url, PHP_URL_HOST);
         $links = $this->crawler->process($url);
 
-        $this->bulkAdd($links);
+        $this->collection->add($url);
+        $this->collection->addAlready($url);
+        
+        $this->bulkAdd($links, $urlCrawlSource);
 
         $depth = $this->depth;
         
         if ($this->collection && $this->collection->links) {
             if (count($this->collection->links) >= $this->crawler->getPageLimit()) {
-                return $this->collection->links;
+                return array_slice($this->collection->links, 0, $this->crawler->getPageLimit());
             }
 
             if($depth > 0)
@@ -67,12 +81,15 @@ class SitemapService
                 while($depth > 0)
                 {
                     foreach($this->collection->links as $link){
-                        if($this->collection->isCrawled($link)){
-                            $links = $this->crawler->process($link);
-                            $this->bulkAdd($links);
+                        $urlCrawlTarget = parse_url($link, PHP_URL_HOST);
+                        if ($urlCrawlTarget === $urlCrawlSource && !$this->collection->isCrawled($link)) {    
+                            $links = $this->crawler->process($link, $urlCrawlSource);
+
+                            $this->bulkAdd($links, $urlCrawlSource);
+                            $this->collection->addAlready($link);
 
                             if ($this->collection && $this->collection->links && count($this->collection->links) >= $this->crawler->getPageLimit()) {
-                                return $this->collection->links;
+                                return array_slice($this->collection->links, 0, $this->crawler->getPageLimit());
                             }
                         }
                     }
@@ -81,7 +98,7 @@ class SitemapService
                 }
             }
 
-            return $this->collection->links;
+            return array_slice($this->collection->links, 0, $this->crawler->getPageLimit());
         } else {
             return [];
         }
